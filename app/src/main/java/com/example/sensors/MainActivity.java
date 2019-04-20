1b013726927d2ba7435042e295cce23ab2827cc0
azarrias <adolfo.zarrias@gmail.com>
2019-04-20 14:32:15 +0200

    Display a list of all sensors present on the device

--- app/src/main/java/com/example/sensors/MainActivity.java
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.sensors;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvOutput = findViewById(R.id.tv_output);
+        tvOutput.setText("");
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor : sensors) {
+            log(sensor.getName());
+        }
+    }
+
+    private void log(String str) {
+        tvOutput.append(str + "\n");
     }
 }
